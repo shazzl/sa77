313f8139d06c9a4186cac504735cc78562ad4bc4
Rachit Rawat <rachit.rc96@gmail.com>
2013-09-21 17:17:01 +0530

    disable gentle fair sleeps and use ARCH_POWER
    Sleeper Fairness is a concept used by CFS which treat sleeping/waiting tasks as if they were in a run queue. This implies tasks which spend most of the time waiting for an user input and such will get a fair share of CPU when they need it. Disabling Gentle Fair Sleepers could improve UI responsiveness. Arch Power patch causes arch dependent power functions to be used instead of generic high resolution timer ticks and double ticks
    
    Signed-off-by: Rachit Rawat <rachit.rc96@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -3,8 +3,8 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
-
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
+s
 /*
  * Place new tasks ahead so that they do not starve already running
  * tasks
@@ -42,7 +42,7 @@ SCHED_FEAT(CACHE_HOT_BUDDY, true)
 /*
  * Use arch dependent cpu power functions
  */
-SCHED_FEAT(ARCH_POWER, false)
+SCHED_FEAT(ARCH_POWER, true)
 
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(DOUBLE_TICK, false)
